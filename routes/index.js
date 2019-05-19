182349c81e7f4b9028f2d108391eb81a33432105
VictorGeorge <victor.georgemattos@gmail.com>
2019-05-18 21:04:30 -0300

    Updated project to PUG, deletion of useless files

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express'); // require Express
 var router = express.Router(); // setup usage of the Express router engine
 
 /* PostgreSQL and PostGIS module and connection setup */
-var pg = require("pg"); // require Postgres module
+const { Client, Query } = require('pg')
+
 // Setup connection
 var username = "postgres" // sandbox username
 var password = "PostRead" // read only privileges on our table
@@ -16,19 +17,17 @@ const queryLimit = 10000;
 var coffee_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente)) As properties FROM accidents  As lg LIMIT " + queryLimit + ") As f) As fc";
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Express'
-    });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
 
 /* GET Postgres JSON data */
 router.get('/data', function (req, res) {
-    var client = new pg.Client(conString);
+    var client = new Client(conString);
     client.connect();
-    var query = client.query(coffee_query);
+    var query = client.query(new Query(coffee_query));
     query.on("row", function (row, result) {
         result.addRow(row);
     });
@@ -40,17 +39,18 @@ router.get('/data', function (req, res) {
 
 /* GET the map page */
 router.get('/map', function(req, res) {
-    var client = new pg.Client(conString);
-    client.connect();
-    var query = client.query(coffee_query);
+    var client = new Client(conString); // Setup our Postgres Client
+    client.connect(); // connect to the client
+    var query = client.query(new Query(coffee_query)); // Run our Query
     query.on("row", function (row, result) {
         result.addRow(row);
     });
+    // Pass the result to the map page
     query.on("end", function (result) {
-        var data = result.rows[0].row_to_json
+        var data = result.rows[0].row_to_json // Save the JSON as variable data
         res.render('map', {
-            title: "Express API",
-            jsonData: data
+            title: "Express API", // Give a title to our page
+            jsonData: data // Pass data to the View
         });
     });
 });
@@ -65,9 +65,9 @@ router.get('/filter*', function (req, res) {
     } else {
         console.log("Request passed")
         var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente)) As properties FROM accidents  As lg WHERE lg.dia_semana = \'" + weekDay + "\' LIMIT " + queryLimit + ") As f) As fc";
-        var client = new pg.Client(conString);
+        var client = new Client(conString);
         client.connect();
-        var query = client.query(filter_query);
+        var query = client.query(new Query(filter_query)); // Run our Query
         query.on("row", function (row, result) {
             result.addRow(row);
         });
