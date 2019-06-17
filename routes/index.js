50e0fd69af9a951c56c32c8bbae2ececa3b496e5
VictorGeorge <victor.georgemattos@gmail.com>
2019-06-17 19:59:32 -0300

    First ChartJS weekday counter

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Yo
 const queryLimit = 10000;
 
 // Set up your database query to display GeoJSON
-var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente)) As properties FROM accidents  As lg LIMIT " + queryLimit + ") As f) As fc";
+var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana)) As properties FROM accidents  As lg LIMIT " + queryLimit + ") As f) As fc";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,6 +25,7 @@ module.exports = router;
 
 /* GET Postgres JSON data */
 router.get('/data', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     var client = new Client(conString);
     client.connect();
     var query = client.query(new Query(accidentsQuery));
@@ -64,7 +65,7 @@ router.get('/filter*', function (req, res) {
         return;
     } else {
         console.log("Request passed")
-        var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente)) As properties FROM accidents  As lg WHERE lg.dia_semana = \'" + weekDay + "\' LIMIT " + queryLimit + ") As f) As fc";
+        var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana)) As properties FROM accidents  As lg WHERE lg.dia_semana = \'" + weekDay + "\' LIMIT " + queryLimit + ") As f) As fc";
         var client = new Client(conString);
         client.connect();
         var query = client.query(new Query(filter_query)); // Run our Query
