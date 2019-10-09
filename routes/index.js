d4dca86a87b0272b63480c70f7578bbb36dba3a1
VictorGeorge <victor.georgemattos@gmail.com>
2019-10-08 21:07:55 -0300

    Colors change

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,10 @@ var host = "localhost:5432"
 var database = "accidents" // database name
 var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
 
-const queryLimit = 50000;
+const queryLimit = 75000;
 
 // Set up your database query to display GeoJSON
-var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano)) As properties FROM accidents  As lg LIMIT " + queryLimit + ") As f) As fc";
+var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano, br)) As properties FROM accidents As lg WHERE lg.br = \'" + 116 + "\' LIMIT " + queryLimit + ") As f) As fc";
 
 //Get the 5 most common causes of accidents
 var causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
@@ -22,10 +22,13 @@ var causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents GROUP B
 //Get the 5 most common hours of accidents
 var hoursQuery = "SELECT extract(hour from horario), COUNT(*)from public.accidents GROUP BY extract(hour from horario) ORDER BY count(*) DESC LIMIT 5";
 
+//Get the 10 most common states
+var statesQuery = "SELECT uf, count(*) numero FROM public.accidents WHERE ano BETWEEN 2007 AND 2017 GROUP BY uf ORDER BY numero DESC LIMIT 10";
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
@@ -46,7 +49,7 @@ router.get('/data', function (req, res) {
 });
 
 /* GET the map page */
-router.get('/map', function(req, res) {
+router.get('/map', function (req, res) {
     var client = new Client(conString); // Setup our Postgres Client
     client.connect(); // connect to the client
     var query = client.query(new Query(accidentsQuery)); // Run our Query
@@ -61,19 +64,24 @@ router.get('/map', function(req, res) {
     queryHours.on("row", function (row, resultHours) {
         resultHours.addRow(row);
     });
+    var queryStates = client.query(new Query(statesQuery)); // Run our causesQuery
+    queryStates.on("row", function (row, resultStates) {
+        resultStates.addRow(row);
+    });
 
     // Pass the result to the map page
     query.on("end", function (result) {
         var data = result.rows[0].row_to_json // Save the JSON as variable data
-
         queryCauses.on("end", function (resultCauses) {
-            
             queryHours.on("end", function (resultHours) {
-                res.render('map', {
-                    title: "Express API", // Give a title to our page
-                    jsonData: data, // Pass data to the View
-                    causesData: resultCauses,
-                    hoursData: resultHours
+                queryStates.on("end", function (resultStates) {
+                    res.render('map', {
+                        title: "Express API", // Give a title to our page
+                        jsonData: data, // Pass data to the View
+                        causesData: resultCauses,
+                        hoursData: resultHours,
+                        statesData: resultStates
+                    });
                 });
             });
         });
@@ -85,8 +93,8 @@ router.get('/map', function(req, res) {
 router.get('/filter*', function (req, res) {
     var weekDay = req.query.weekDay;
     var yearAccident = req.query.yearAccident;
-    if(weekDay != undefined){
-        if (weekDay.indexOf("--") > -1 || weekDay.indexOf("'") > -1 || weekDay.indexOf(";") > -1 || weekDay.indexOf("/*") > -1 || weekDay.indexOf("xp_") > -1){
+    if (weekDay != undefined) {
+        if (weekDay.indexOf("--") > -1 || weekDay.indexOf("'") > -1 || weekDay.indexOf(";") > -1 || weekDay.indexOf("/*") > -1 || weekDay.indexOf("xp_") > -1) {
             console.log("Bad request detected");
             res.redirect('/map');
             return;
@@ -126,8 +134,8 @@ router.get('/filter*', function (req, res) {
             });
         };
     }
-    else if(yearAccident != undefined){
-        if (yearAccident.indexOf("--") > -1 || yearAccident.indexOf("'") > -1 || yearAccident.indexOf(";") > -1 || yearAccident.indexOf("/*") > -1 || yearAccident.indexOf("xp_") > -1){
+    else if (yearAccident != undefined) {
+        if (yearAccident.indexOf("--") > -1 || yearAccident.indexOf("'") > -1 || yearAccident.indexOf(";") > -1 || yearAccident.indexOf("/*") > -1 || yearAccident.indexOf("xp_") > -1) {
             console.log("Bad request detected");
             res.redirect('/map');
             return;
