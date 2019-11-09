65d0b5330df2447a8da32f8970a2da1d57742eee
VictorGeorge <victor.georgemattos@gmail.com>
2019-11-09 17:12:58 -0300

    Multiple filter of cluster data working

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' A
 var causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
 
 //Get the 5 most common hours of accidents
-var hoursQuery = "SELECT extract(hour from horario), COUNT(*)from public.accidents WHERE ano = 2018 GROUP BY extract(hour from horario) ORDER BY count(*) DESC LIMIT 5";
+var hoursQuery = "SELECT hora, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY hora ORDER BY count(*) DESC LIMIT 5";
 
 //Get the 10 most common states
 var statesQuery = "SELECT uf, count(*) numero FROM public.accidents WHERE ano BETWEEN 2007 AND 2017 GROUP BY uf ORDER BY numero DESC LIMIT 10";
@@ -27,7 +27,7 @@ var statesQuery = "SELECT uf, count(*) numero FROM public.accidents WHERE ano BE
 //Get the 10 Br's with most accidents
 var brsQuery = "SELECT br, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY br ORDER BY count(*) DESC LIMIT 5";
 
-var mainQueryPrefix = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano)) As properties FROM accidents  As lg ";
+var mainQueryPrefix = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano, causa_acidente, horario)) As properties FROM accidents  As lg ";
 
 var mainQuerySuffix = ") As f) As fc";
 
@@ -108,17 +108,22 @@ router.get('/filter*', async function (req, res) {
 
     if (weekDay != undefined) {
         if (isRequestValid(weekDay) === true) {
-            accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano)) As properties FROM accidents  As lg WHERE lg.dia_semana = \'" + weekDay + "\' LIMIT " + queryLimit + ") As f) As fc";
+            let weekdayFilterString = await weekdayFilter(weekDay);
+            filters = filters.concat(weekdayFilterString);
         }
     }
     if (causesAccident != undefined) {
         if (isRequestValid(causesAccident) === true) {
-            var causesAccident = weekdayFilter(causesAccident);
+            causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents WHERE ano = 2018 AND causa_acidente = \'" + causesAccident + "\' GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
+            let causesFilterString = await causesFilter(causesAccident);
+            filters = filters.concat(causesFilterString);
         }
     }
     if (hoursAccident != undefined) {
         if (isRequestValid(hoursAccident) === true) {
-            var weekdayQuery = weekdayFilter(weekDay);
+            hoursQuery = "SELECT hora, COUNT(*) FROM public.accidents WHERE ano = 2018 AND hora = " + hoursAccident + " GROUP BY hora ORDER BY count(*) DESC LIMIT 5"; 
+            let hoursString = await hoursFilter(hoursAccident);
+            filters = filters.concat(hoursString);
         }
     }
     if (brsAccident != undefined) {
@@ -151,14 +156,19 @@ async function yearFilter(yearAccident) {
 }
 
 async function weekdayFilter(weekDay) {
-    console.log("Request passed");
-    var causesQueryFiltered = "AND lg.dia_semana = \'" + weekDay + "\' ";
-
-    return result;
+    return "AND lg.dia_semana = \'" + weekDay + "\' ";
 }
 
 async function brFilter(brsAccident) {
     return "AND lg.br = \'" + brsAccident + "\' ";
+}
+
+async function causesFilter(causes) {
+    return "AND lg.causa_acidente = \'" + causes + "\' ";
+}
+
+async function hoursFilter(hour) {
+    return "AND lg.hora = \'" + hour + "\' ";
 }
 
 
