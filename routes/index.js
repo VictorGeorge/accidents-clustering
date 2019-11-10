f367cc53336345bcf8bca854f6e4291b967509d4
VictorGeorge <victor.georgemattos@gmail.com>
2019-11-10 20:44:17 -0300

    Multifilter almost complete

--- routes/index.js
+++ routes/index.js
@@ -15,24 +15,29 @@ const queryLimit = 12000;
 
 // Set up your database query to display GeoJSON
 var accidentsQuery = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano, br)) As properties FROM public.accidents As lg WHERE lg.ano = \'" + 2018 + "\' LIMIT " + queryLimit + ") As f) As fc";
+
 //Get the 5 most common causes of accidents
 var causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
+var causesQueryPrefix = "SELECT causa_acidente, COUNT(*) FROM public.accidents";
+var causesQuerySuffix = " GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
 
 //Get the 5 most common hours of accidents
 var hoursQuery = "SELECT hora, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY hora ORDER BY count(*) DESC LIMIT 5";
+var hoursQueryPrefix = "SELECT hora, COUNT(*) FROM public.accidents";
+var hoursQuerySuffix = " GROUP BY hora ORDER BY count(*) DESC LIMIT 5";
 
 //Get the 10 most common states
 var statesQuery = "SELECT uf, count(*) numero FROM public.accidents WHERE ano BETWEEN 2007 AND 2017 GROUP BY uf ORDER BY numero DESC LIMIT 10";
+var statesQueryPrefix = "SELECT uf, count(*) numero FROM public.accidents";
+var statesQuerySuffix = " GROUP BY uf ORDER BY numero DESC LIMIT 10";
 
 //Get the 10 Br's with most accidents
 var brsQuery = "SELECT br, COUNT(*) FROM public.accidents WHERE ano = 2018 GROUP BY br ORDER BY count(*) DESC LIMIT 5";
+var brsQueryPrefix = "SELECT br, COUNT(*) FROM public.accidents";
+var brsQuerySuffix = " GROUP BY br ORDER BY count(*) DESC LIMIT 5";
 
 var mainQueryPrefix = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, data_inversa, classificacao_acidente, dia_semana, ano, causa_acidente, horario)) As properties FROM accidents  As lg ";
-
 var mainQuerySuffix = ") As f) As fc";
-
-
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -71,11 +76,10 @@ async function doQueries(req, res) {
     const resultCauses = await client.query(causesQuery);
     const resultHours = await client.query(hoursQuery);
     const resultStates = await client.query(statesQuery);
-    //console.log(brsQuery);
     const resultBrs = await client.query(brsQuery);
-    //console.log(resultBrs);
+
     await client.end();
-    res.render('map', {
+    await res.render('map', {
         title: "Accidents API", // Give a title to our page
         jsonData: data, // Pass data to the View
         causesData: resultCauses,
@@ -89,6 +93,7 @@ async function doQueries(req, res) {
 /* GET the filtered page */
 router.get('/filter*', async function (req, res) {
     var filters;
+    var filtersSelects;
     var weekDay = req.query.weekDay;
     var yearAccident = req.query.yearAccident;
     var causesAccident = req.query.causesAccident;
@@ -99,24 +104,37 @@ router.get('/filter*', async function (req, res) {
     if (yearAccident != undefined) {
         if (isRequestValid(yearAccident) === true) {
             filters = await yearFilter(yearAccident);
+            filtersSelects = await yearFilterSelects(yearAccident);
         }
-        else
+        else{
             filters = await yearFilter(2018);
+            filtersSelects = await yearFilterSelects(2018);
+        }
     }
-    else
+    else{
         filters = await yearFilter(2018);
+        filtersSelects = await yearFilterSelects(2018);
+    }
 
     if (weekDay != undefined) {
         if (isRequestValid(weekDay) === true) {
             let weekdayFilterString = await weekdayFilter(weekDay);
             filters = filters.concat(weekdayFilterString);
+
+            weekdayFilterString = await weekdayFilterSelects(weekDay);
+            filtersSelects = filtersSelects.concat(weekdayFilterString);
+
         }
     }
     if (causesAccident != undefined) {
         if (isRequestValid(causesAccident) === true) {
+            causesAccident = causesAccident.trim();
             causesQuery = "SELECT causa_acidente, COUNT(*) FROM public.accidents WHERE ano = 2018 AND causa_acidente = \'" + causesAccident + "\' GROUP BY causa_acidente ORDER BY count(*) DESC LIMIT 5";
             let causesFilterString = await causesFilter(causesAccident);
             filters = filters.concat(causesFilterString);
+
+            causesFilterString = await causesFilterSelects(causesAccident);
+            filtersSelects = filtersSelects.concat(causesFilterString);
         }
     }
     if (hoursAccident != undefined) {
@@ -124,6 +142,9 @@ router.get('/filter*', async function (req, res) {
             hoursQuery = "SELECT hora, COUNT(*) FROM public.accidents WHERE ano = 2018 AND hora = " + hoursAccident + " GROUP BY hora ORDER BY count(*) DESC LIMIT 5"; 
             let hoursString = await hoursFilter(hoursAccident);
             filters = filters.concat(hoursString);
+
+            hoursString = await hoursFilterSelects(hoursAccident);
+            filtersSelects = filtersSelects.concat(hoursString);
         }
     }
     if (brsAccident != undefined) {
@@ -131,11 +152,19 @@ router.get('/filter*', async function (req, res) {
             brsQuery = "SELECT br, COUNT(*) FROM public.accidents WHERE ano = 2018 AND br = \'" + brsAccident + "\' GROUP BY br ORDER BY count(*) DESC LIMIT 1";//TODO
             let brFilterString = await brFilter(brsAccident);
             filters = filters.concat(brFilterString);
+
+            brFilterString = await brFilterSelects(brsAccident);
+            filtersSelects = filtersSelects.concat(brFilterString);
         }
     }
     var filterString = mainQueryPrefix;
     filterString = filterString.concat(filters, mainQuerySuffix);
     accidentsQuery = filterString;
+
+    causesQuery = causesQueryPrefix + filtersSelects + causesQuerySuffix;
+    hoursQuery = hoursQueryPrefix + filtersSelects + hoursQuerySuffix;
+    statesQuery = statesQueryPrefix + filtersSelects + statesQuerySuffix;
+    brsQuery = brsQueryPrefix + filtersSelects + brsQuerySuffix;
     doQueries(req, res);
 });
 
@@ -152,23 +181,43 @@ function isRequestValid(request) {
 }
 
 async function yearFilter(yearAccident) {
-    return "WHERE lg.ano = \'" + yearAccident + "\' ";
+    return "WHERE lg.ano = " + yearAccident;
 }
 
 async function weekdayFilter(weekDay) {
-    return "AND lg.dia_semana = \'" + weekDay + "\' ";
+    return " AND lg.dia_semana = \'" + weekDay + "\'";
 }
 
 async function brFilter(brsAccident) {
-    return "AND lg.br = \'" + brsAccident + "\' ";
+    return " AND lg.br = \'" + brsAccident + "\' ";
 }
 
 async function causesFilter(causes) {
-    return "AND lg.causa_acidente = \'" + causes + "\' ";
+    return " AND lg.causa_acidente = \'" + causes + "\'";
 }
 
 async function hoursFilter(hour) {
-    return "AND lg.hora = \'" + hour + "\' ";
+    return " AND lg.hora = " + hour;
 }
 
+
+async function yearFilterSelects(yearAccident) {
+    return " WHERE ano = " + yearAccident;
+}
+
+async function weekdayFilterSelects(weekDay) {
+    return " AND dia_semana = \'" + weekDay + "\' ";
+}
+
+async function brFilterSelects(brsAccident) {
+    return " AND br = \'" + brsAccident + "\' ";
+}
+
+async function causesFilterSelects(causes) {
+    return " AND causa_acidente = \'" + causes + "\' ";
+}
+
+async function hoursFilterSelects(hour) {
+    return " AND hora = " + hour;
+}
 
